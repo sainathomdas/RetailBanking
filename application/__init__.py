a52185631451c7da9cc8c2047450ce4915571625
sainathomdas <sainathomdas@gmail.com>
2020-06-13 16:20:43 +0530

    Initial Commit

---
+++ application/__init__.py
@@ -0,0 +1,6 @@
+from flask import Flask
+from config import Config
+app = Flask(__name__,template_folder = 'templates', static_folder= 'static')
+
+app.config.from_object(Config)
+from application.routes import login_route
---
+++ application/routes/login_route.py
@@ -0,0 +1,21 @@
+from application import app
+from flask import render_template, request, redirect, url_for, flash
+
+@app.route('/')
+@app.route('/login/',methods = ['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if(len(username) < 8 ):
+            flash("Username must contain a minimum of 8 characters", "warning")
+            return redirect(url_for('login'))
+        if(len(password) < 8):
+            flash("Password must contain a minimum of 10 characters", "warning")
+            return redirect(url_for('login'))
+    return render_template('login.html', login_page = True)
+
+
+@app.route('/create_customer')
+def createCustomer():
+    return render_template('create_customer.html')
