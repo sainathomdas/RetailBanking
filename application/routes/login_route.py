f92e9f9e7746558ad0be8e300167667b1d51e122
sainathomdas <sainathomdas@gmail.com>
2020-06-15 14:06:27 +0530

    changed login.html

--- application/routes/login_route.py
+++ application/routes/login_route.py
@@ -7,12 +7,8 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        if(len(username) < 8 ):
-            flash("Username must contain a minimum of 8 characters", "warning")
-            return redirect(url_for('login'))
-        if(len(password) < 8):
-            flash("Password must contain a minimum of 10 characters", "warning")
-            return redirect(url_for('login'))
+        login_type = request.form['login_type']
+        
     return render_template('login.html', login_page = True)
 
 
@@ -20,14 +16,21 @@ def login():
 def createCustomer():
     return render_template('create_customer.html', customer_mgmt = True)
     
-@app.route('/update_customer')
-def updateCustomer():
-    return render_template('update_customer.html')
+@app.route('/update_customer/', methods = ['GET', 'POST'])
+def updateCustomer(data = False):
+    if 'data' in request.args:
+        data = request.args['data']
+        data = eval(data)
+    return render_template('update_customer.html', data = data)
+            
 
-
-@app.route('/delete_customer')
+@app.route('/delete_customer/', methods = ['GET', 'POST'])
 def deleteCustomer():
     return render_template('delete_customer.html')
+
+@app.route('/view_customer', methods = ['GET', 'POST'])
+def viewCustomer():
+    return render_template('view_customer.html', datatable = True, customer_mgmt = True)
 
 
 @app.route('/customer_status')
@@ -37,3 +40,21 @@ def customerStatus():
 @app.route('/customer_management')
 def customerManagement():
     return render_template('customer_mgmt.html', datatable = True, customer_mgmt = True)
+
+@app.route('/search_customer', methods = ['GET', 'POST'])
+def searchCustomer():
+    if request.method == 'POST':
+        if(request.form['input_type'] and request.form['id'] and request.form['redirect_url']):
+            input_type = request.form['input_type']
+            id = request.form['id']
+            from_page = request.form['from_page']
+            redirect_url = request.form['redirect_url']
+            if 1==1:
+                #if customer found pull data from DB and return
+                return redirect(url_for(redirect_url, data = {'name':'sainath', 'age': 21}),code=307)
+            else:
+                flash(f"Customer with {input_type} = {id} not found", category= 'warning')
+                return redirect(url_for(redirect_url))
+    
+
+
