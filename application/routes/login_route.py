14b0cfcd26c3efb0a184d2e9b802499647b53ac4
sainathomdas <sainathomdas@gmail.com>
2020-06-15 14:41:21 +0530

    update

--- application/routes/login_route.py
+++ application/routes/login_route.py
@@ -11,16 +11,40 @@ def login():
         
     return render_template('login.html', login_page = True)
 
-
-@app.route('/create_customer')
+@app.route('/create_customer/')
 def createCustomer():
     return render_template('create_customer.html', customer_mgmt = True)
     
 @app.route('/update_customer/', methods = ['GET', 'POST'])
-def updateCustomer(data = False):
-    return render_template('update_customer.html')
+def updateCustomer():
+    if request.method == "POST":
+        if(request.form['input_type'] and request.form['id']):
+            input_type = request.form['input_type']
+            id = request.form['id']
+            #search for customer data with id and input_type
+            if 1==1: #if customer found
+                return render_template('update_customer.html',search = False, data = 'sainath')
+            else:
+                flash(f"Customer with {input_type} = {id} not found!", category='warning')
 
-            
+    return render_template('update_customer.html', search = True)
+
+@app.route('/update_into_database', methods = ['GET', 'POST'])
+def updateIntoDatabase():
+    if request.method == 'POST':
+        cust_ssid = request.form['cust_ssid']
+        cust_id = request.form['cust_id']
+        cust_old_name = request.form['cust_old_name']
+        cust_new_name = request.form['cust_new_name']
+        cust_old_age = request.form['cust_old_age']
+        cust_new_age = request.form['cust_new_age']
+        cust_new_address = request.form['cust_new_address']
+        cust_state = request.form['cust_state']
+        cust_city = request.form['cust_city']
+
+
+
+
 
 @app.route('/delete_customer/', methods = ['GET', 'POST'])
 def deleteCustomer():
@@ -29,7 +53,6 @@ def deleteCustomer():
 @app.route('/view_customer', methods = ['GET', 'POST'])
 def viewCustomer():
     return render_template('view_customer.html', datatable = True, customer_mgmt = True)
-
 
 @app.route('/customer_status')
 def customerStatus():
