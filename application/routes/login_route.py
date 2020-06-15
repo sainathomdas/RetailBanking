a4eddc96a731016d47e156e94e8cb3fe25ee3670
sainathomdas <sainathomdas@gmail.com>
2020-06-15 15:02:13 +0530

    Merged arun code

--- application/routes/login_route.py
+++ application/routes/login_route.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from passlib.hash import sha256_crypt
 from functools import wraps
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Database.db'
 db = SQLAlchemy(app)
 
@@ -22,22 +21,14 @@ class ExecutiveAccount(db.Model):
 # if(sha256_crypt.verify(password_candidate,password))
 
 @app.route('/')
-def base_page():
-    return '<h3>welcome to root page</h3><br><a href="/login">GO TO LOGIN</a>'
-
 @app.route('/login/',methods = ['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        user_name = request.form['username']
-        password_candidate = request.form['password']
-        if(len(user_name) < 8 ):
-            flash("Username must contain a minimum of 8 characters", "warning")
-            return redirect(url_for('login'))
-        if(len(password_candidate) < 8):
-            flash("Password must contain a minimum of 10 characters", "warning")
-            return redirect(url_for('login'))
+        username = request.form['username']
+        password = request.form['password']
+        login_type = request.form['login_type']
         
-        result = db.session.query(ExecutiveAccount).filter(ExecutiveAccount.username==user_name)
+    result = db.session.query(ExecutiveAccount).filter(ExecutiveAccount.username==user_name)
         
         if(len(result.all())>0):
             for row in result:
@@ -76,27 +67,61 @@ def logout():
     return render_template('login.html')
 
 
-@app.route('/create_customer')
+
+@app.route('/create_customer/')
 @is_logged_in
 def createCustomer():
-    return render_template('create_customer.html')
+    return render_template('create_customer.html', customer_mgmt = True)
     
-
-@app.route('/update_customer')
+@app.route('/update_customer/', methods = ['GET', 'POST'])
 @is_logged_in
 def updateCustomer():
-    return render_template('update_customer.html')
+    if request.method == "POST":
+        if(request.form['input_type'] and request.form['id']):
+            input_type = request.form['input_type']
+            id = request.form['id']
+            #search for customer data with id and input_type
+            if 1==1: #if customer found
+                return render_template('update_customer.html',search = False, data = 'sainath')
+            else:
+                flash(f"Customer with {input_type} = {id} not found!", category='warning')
+
+    return render_template('update_customer.html', search = True)
+
+@app.route('/update_into_database', methods = ['GET', 'POST'])
+@is_logged_in
+def updateIntoDatabase():
+    if request.method == 'POST':
+        cust_ssid = request.form['cust_ssid']
+        cust_id = request.form['cust_id']
+        cust_old_name = request.form['cust_old_name']
+        cust_new_name = request.form['cust_new_name']
+        cust_old_age = request.form['cust_old_age']
+        cust_new_age = request.form['cust_new_age']
+        cust_new_address = request.form['cust_new_address']
+        cust_state = request.form['cust_state']
+        cust_city = request.form['cust_city']
 
 
-@app.route('/delete_customer')
+@app.route('/delete_customer/', methods = ['GET', 'POST'])
 @is_logged_in
 def deleteCustomer():
     return render_template('delete_customer.html')
 
+@app.route('/view_customer', methods = ['GET', 'POST'])
+@is_logged_in
+def viewCustomer():
+    return render_template('view_customer.html', datatable = True, customer_mgmt = True)
 
-"""
-cd ~
-source webflask/bin/activate
-cd Documents/Flask\ Apps/RetailBanking
-python app.py
-"""
+@app.route('/customer_status')
+@is_logged_in
+def customerStatus():
+    return render_template('customer_status.html', datatable = True)
+
+@app.route('/customer_management')
+@is_logged_in
+def customerManagement():
+    return render_template('customer_mgmt.html', datatable = True, customer_mgmt = True)
+
+
+
